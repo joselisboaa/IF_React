b690a0d0a0586723411be30a5834375e2a8a7a1b
1mmanuelKant <joselisboaa05@gmail.com>
2022-08-24 20:55:49 -0300

    prevented default submission

--- src/components/body/fields/input.jsx
+++ src/components/body/fields/input.jsx
@@ -1,28 +1,16 @@
 import styles from './input.module.css'
 
-const Form = (props) => {
+const Form = () => {
 
-  // const [search, setSearch] = useState();
-  // const { register, handleSubmit, formState: { errors } } = useForm();
-
-  // const getStudent = async () => {
-  //   const response = await fetch("http://localhost:5173/api/students.json");
-  //   const result = await response.json();
-
-  //   return result;
-  // }
-
-
-  // const handleSearch = searchResult => searchResult.search ===
-  // (searchResult.search === getStudent().then(data => data.students.filter(infos => Object.values(infos).includes(searchResult.search) && setSearch(searchResult.search))))
-
-  // console.log(search)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
 
   return(
     <div className={styles.container_form}>
-      <form /*onSubmit={handleSubmit(handleSearch)}*/>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Email, matrícula ou nome..."
-        name="search" /*{...register("search")}*/ autoComplete='off' className={styles.search_btn} />
+        name="search" autoComplete='off' className={styles.search_btn} />
         <div className={styles.btn_container}>
           <input type="submit" value="PESQUISAR" className={styles.btn}/>
           <input type="button" value="FAZER PROTOCOLO" className={styles.btn}/>
